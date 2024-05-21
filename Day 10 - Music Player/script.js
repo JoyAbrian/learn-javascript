6f955d4b84647a097c648e9b4c600673187cb072
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-05-21 14:52:41 +0800

    day 10 - music player

---
+++ Day 10 - Music Player/script.js
@@ -0,0 +1,33 @@
+let progress = document.getElementById('progress');
+let song = document.getElementById('song');
+let ctrlIcon = document.getElementById('ctrlIcon');
+
+song.onloadedmetadata = function() {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+function playPause() {
+    if (ctrlIcon.classList.contains("bi-pause-fill")) {
+        song.pause();
+        ctrlIcon.classList.remove("bi-pause-fill")
+        ctrlIcon.classList.add("bi-play-fill")
+    } else {
+        song.play();
+        ctrlIcon.classList.remove("bi-play-fill")
+        ctrlIcon.classList.add("bi-pause-fill")
+    }
+}
+
+if (song.play()) {
+    setInterval(() => {
+        progress.value = song.currentTime;
+    }, 200)
+}
+
+progress.onchange = function() {
+    song.play();
+    song.currentTime = progress.value;
+    ctrlIcon.classList.remove("bi-play-fill")
+    ctrlIcon.classList.add("bi-pause-fill")
+}
