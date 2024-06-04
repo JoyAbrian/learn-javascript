fdc13fe15ebf0516a33640125cdc32d323651be5
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-06-05 00:53:33 +0800

    day 30 - image search

---
+++ Day 30 - Image Search/script.js
@@ -0,0 +1,6 @@
+const searchForm = document.getElementById('search-form');
+const searchBox = document.getElementById('search-box');
+const searchResult = document.getElementById('search-result');
+const showMoreBtn = document.getElementById('show-more-btn');
+
+// I DONT HAVE ANY API KEY TO USE IN THIS PROJECT.
